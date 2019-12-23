66d31986ab28778a1ed84ffd74901638f1bdf7c7
sorshireality <tutunikssa@gmail.com>
2019-12-23 16:40:52 +0200

    add WeChat Payment Method

---
+++ plugins/hikashoppayment/emspaywechat/emspaywechat.php
@@ -0,0 +1,83 @@
+<?php
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ *   ╲          ╱
+ * ╭──────────────╮  COPYRIGHT (C) 2019 GINGER PAYMENTS B.V.
+ * │╭──╮      ╭──╮│
+ * ││//│      │//││  This software is released under the terms of the
+ * │╰──╯      ╰──╯│  MIT License.
+ * ╰──────────────╯
+ *   ╭──────────╮    https://www.gingerpayments.com/
+ *   │ () () () │
+ *
+ * @category    Ginger
+ * @package     Ginger HikaShop
+ * @author      Ginger Payments B.V. (plugins@gingerpayments.com)
+ * @version     v1.3.0
+ * @copyright   COPYRIGHT (C) 2019 GINGER PAYMENTS B.V.
+ * @license     The MIT License (MIT)
+ * @since       v1.0.0
+ **/
+
+JImport('emspay.emspayplugin');
+
+class plgHikashoppaymentEmspayWechat extends EmspayPlugin
+{
+    var $name = 'emspaywechat';
+
+    /**
+     * @param $element
+     * @since v1.0.0
+     */
+    public function getPaymentDefaultValues(&$element)
+    {
+        $element->payment_name = JText::_('PLG_HIKASHOPPAYMENT_EMSPAYWECHAT_NAME');
+        $element->payment_description =  JText::_('PLG_HIKASHOPPAYMENT_EMSPAYWECHAT_DESCRIPTION');
+        $element->payment_params->address_type = 'billing';
+        $element->payment_params->notification = 1;
+        $element->payment_params->invalid_status = 'cancelled';
+        $element->payment_params->verified_status = 'confirmed';
+    }
+
+    /**
+     * @return array
+     * @since v1.0.0
+     */
+    protected function createEmspayOrder()
+    {
+        EmspayHelper::clearKlarnaSessionData();
+
+        $currency = $this->currency->currency_code;
+        $totalInCents = EmspayHelper::getAmountInCents($this->order->order_full_price);
+        $orderId = $this->order->order_id;
+        $description = JFactory::getConfig()->get('sitename').' #'.$orderId;
+        $returnUrl = $this->pluginConfig['notify_url'][2].'&merchant_order_id='.$orderId;
+        $customer = EmspayHelper::getCustomerInfo($this->user,$this->order);
+        $plugin = ['plugin' => EmspayHelper::getPluginVersion($this->name)];
+        $ginger = \Ginger\Ginger::createClient(EmspayHelper::GINGER_ENDPOINT,
+            $this->payment_params->api_key,
+            $this->payment_params->bundle_cacert === '1' ?
+                [
+                    CURLOPT_CAINFO => EmspayHelper::getCaCertPath()
+                ] : []
+        );
+        return $ginger->createOrder([
+            'merchant_order_id' => (string)$orderId,
+            'customer' => $customer,
+            'extra' => $plugin,
+            'currency' => $currency,
+            'amount' => $totalInCents,
+            'description' => $description,
+            'return_url' => $returnUrl,
+            'transactions' => [
+                [
+                    'payment_method' => 'wechat',
+                ]
+            ],
+
+        ]);
+    }
+
+}
